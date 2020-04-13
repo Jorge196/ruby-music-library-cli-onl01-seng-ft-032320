99ea6ddf7b6faed76921e2877da6a0607468ca39
Jorge Ramirez <joramirez96@gmail.com>
2020-04-13 00:02:16 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -48,6 +48,9 @@ class Song
   artist = Artist.find_or_create_by_name(artist_name)
   genre = Genre.find_or_create_by_name(genre_name)
   self.new(song_name, artist, genre)
-end 
+end
+
+  def self.create_from_filename
+    
 
 end
