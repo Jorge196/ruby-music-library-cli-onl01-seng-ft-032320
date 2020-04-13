3a94d92fd0966ba6bc748fe8109925223d96b45b
Jorge Ramirez <joramirez96@gmail.com>
2020-04-13 00:26:39 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -12,7 +12,9 @@ class MusicLibraryController
     if input != exit
 
       puts "Welcome to your music library!"
-      
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To"
 
 
 end
